46528eeef1d7397b3e412a1fd54ae9bc146d0791
Boring-Mind <55541040+Boring-Mind@users.noreply.github.com>
2020-02-16 09:06:27 -0800

    Create python_install.sh

---
+++ python_install.sh
@@ -0,0 +1,7 @@
+# python requirements
+sudo apt-get install gcc libpq-dev -y
+sudo apt-get install python-dev  python-pip -y
+sudo apt-get install python3-dev python3-pip python3-venv python3-wheel -y
+pip3 install wheel
+# fix for "failed building wheel for <module_name>"
+pip install --upgrade pip
